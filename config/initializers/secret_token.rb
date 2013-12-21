0c2b2f4541f4ef1ff542cd286c162596d71136c6
karmaged <krmelnik@gmail.com>
2013-12-21 16:05:34 +0200

    setup Rails 4.0.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PmoComUa::Application.config.secret_key_base = '50f8720b2ce8997738db10b3c94eb13fec7f6f81a926f6fad71900506cf2bbb2a81935802db800ccb4e7124908a05c921bef59e8b6fb0a23422446b1a5a516d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PmoComUa::Application.config.session_store :cookie_store, key: '_pmo_com_ua_session'
